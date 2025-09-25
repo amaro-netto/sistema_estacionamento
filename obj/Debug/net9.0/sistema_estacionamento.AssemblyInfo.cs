b97f698cf6b482bc6eada4186b9b60d192e182db
amaro-netto <amarosilva002@gmail.com>
2025-09-25 20:18:36 -0300

    Conclusão da 1ª parte do projeto.

--- obj/Debug/net9.0/sistema_estacionamento.AssemblyInfo.cs
+++ obj/Debug/net9.0/sistema_estacionamento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sistema_estacionamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03f83157ec7652c31af0ac44026034b456ed05f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d7277d29ee3cf7dd0c2f3d13451bafde5c4691")]
 [assembly: System.Reflection.AssemblyProductAttribute("sistema_estacionamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sistema_estacionamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
